f88f632599f57df99566b2e2be51e994fe5884a8
WatanabeYuito21 <watanabe.yuito21@gmail.com>
2025-07-31 17:40:50 +0900

    fix: Counterを構造体に格納

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,35 @@
 use iced::Element;
 use iced::widget::{button, column, row, text};
 
+#[derive(Default)]
+struct Counter {
+    value: u64,
+}
+
 #[derive(Debug, Clone)]
 enum Message {
     Increment,
 }
 
-fn view(counter: &u64) -> Element<Message> {
+fn view(counter: &Counter) -> Element<Message> {
     // button(text(counter)).on_press(Message::Increment).into()
     column![
         text("This is Text"),
         row![
-            text(counter.to_string()),
+            text(counter.value.to_string()),
             button("This is button").on_press(Message::Increment)
         ]
     ]
+    .spacing(10)
     .into()
 }
 
-fn update(counter: &mut u64, message: Message) {
+fn update(counter: &mut Counter, message: Message) {
     match message {
-        Message::Increment => *counter += 1,
+        Message::Increment => counter.value += 1,
     }
 }
 
 pub fn main() -> iced::Result {
-    iced::run("title", update, view)
+    iced::run("This is title", update, view)
 }
