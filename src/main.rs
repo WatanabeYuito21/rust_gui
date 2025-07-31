6369a8a424a4e8f0ad659ace2d0b9e14aba3d117
WatanabeYuito21 <watanabe.yuito21@gmail.com>
2025-07-31 17:12:49 +0900

    add: first commit, Hello World

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use iced::Element;
+use iced::widget::{button, text};
+
+#[derive(Debug, Clone)]
+enum Message {
+    Increment,
+}
+
+fn view(counter: &u64) -> Element<Message> {
+    button(text(counter)).on_press(Message::Increment).into()
+}
+
+fn update(counter: &mut u64, message: Message) {
+    match message {
+        Message::Increment => *counter += 1,
+    }
+}
+
+pub fn main() -> iced::Result {
+    iced::run("title", update, view)
+}
